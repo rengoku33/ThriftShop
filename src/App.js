de325fa98b7d1079b863b1eac58a69519e589f89
Kiran Vignesh <rajkiranvignesh369@gmail.com>
2024-03-09 19:01:54 +0530

    categories update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import './Categories.styles.scss';
 
-function App() {
+
+const App = () => {
+  const categories = [
+    {
+      id: 1,
+      title: 'Hats',
+      imageURL: 'https://raw.githubusercontent.com/rengoku33/ThriftShop/main/src/images/cap.jpg',
+    },
+    {
+      id: 2,
+      title: 'Jackets',
+      imageURL: 'https://raw.githubusercontent.com/rengoku33/ThriftShop/main/src/images/jacket.png',
+    },
+    {
+      id: 3,
+      title: 'Sneakers',
+      imageURL: 'https://raw.githubusercontent.com/rengoku33/ThriftShop/main/src/images/shoes.png',
+    },
+    {
+      id: 4,
+      title: 'Womens',
+      imageURL: 'https://raw.githubusercontent.com/rengoku33/ThriftShop/main/src/images/womens.png',
+    },
+    {
+      id: 5,
+      title: 'Mens',
+      imageURL: 'https://raw.githubusercontent.com/rengoku33/ThriftShop/main/src/images/mens.png',
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
+    <div className='categories-container'>
+      {categories.map(({ title, id, imageURL }) => (
+        <div key={id} className='category-container'>
+          <div 
+            className='background-image' style={{
+            backgroundImage: `url(${imageURL})`
+          }}/>
+          <div className='category-body-container'>
+            <h2>{title}</h2>
+            <p>Shop Now</p>
+          </div>
+        </div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
